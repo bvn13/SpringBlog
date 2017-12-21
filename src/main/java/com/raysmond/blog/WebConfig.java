162c42d913d457000157edcaebed715fa29db41b
Vyacheslav N. Boyko <mail4bvn@gmail.com>
2017-12-22 00:02:20 +0300

    fixed webjars loading

--- src/main/java/com/raysmond/blog/WebConfig.java
+++ src/main/java/com/raysmond/blog/WebConfig.java
@@ -36,7 +36,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/webjars/**")
-                .addResourceLocations("/webjars/");
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Override
