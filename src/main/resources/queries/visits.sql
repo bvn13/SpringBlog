53d8091a0f01d684408afd361b97536a704ebc4b
Vyacheslav N. Boyko <mail4bvn@gmail.com>
2017-12-21 12:12:49 +0300

    query updated

--- src/main/resources/queries/visits.sql
+++ src/main/resources/queries/visits.sql
@@ -1,11 +1,12 @@
 
-
-
-select v.post_id, count(distinct v.clientip)
+select date_trunc('day', v.createdat) as dt, v.post_id, p.title, count(distinct v.clientip) as count
 from visits as v
-left join seo_robots_agents as ra
-on case when ra.isregexp then lower(nullif(v.useragent,'')) ~* lower(ra.useragent)
-else lower(nullif(v.useragent,'')) like concat('%',lower(ra.useragent),'%') end
+  left join seo_robots_agents as ra
+    on case when ra.isregexp then lower(nullif(v.useragent,'')) ~* lower(ra.useragent)
+       else lower(nullif(v.useragent,'')) like concat('%',lower(ra.useragent),'%') end
+  inner join posts as p
+    on v.post_id = p.id
 where v.isadmin = false
-and ra.id isnull
-group by v.post_id
+      and ra.id isnull
+group by date_trunc('day', v.createdat), v.post_id, p.title
+order by date_trunc('day', v.createdat), v.post_id
